d5d4e2b4cb851942f1a44e179a228f24b7dba2de
S1161110 <s1161110@gm.ncue.edu.tw>
2025-10-22 03:46:16 +0000

    whynot

--- page_map.py
+++ page_map.py
@@ -1,11 +1,29 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
-st.set_page_config(layout="wide") # Wÿ¿¯
-st.title("Leafmap ×W½")
-# 1. W{þ (m)
-# center=[, ], zoom=
-m = leafmap.Map(center=[24.0, 121.0], zoom=7) # 
-# 2. (ÿ)  m Nò/W
-m.add_basemap("OpenTopoMap") # W (OpenTopoMap )
-# 3. W{þ Streamlit
-m.to_streamlit(height=600)
+import geopandas as gpd
+st.set_page_config(layout="wide")
+st.title("Leafmap - Uß (Vector) + | (Raster)")
+# --- 1. | (COG) ---
+# Ï:Nö SRTM DEM (|ß)
+cog_url = "https://github.com/opengeos/leafmap/raw/master/examples/data/cog.tif"
+# --- 2. Uß (GDF) ---
+url = "https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_0_countries.zip"
+gdf = gpd.read_file(url)
+# --- 3. W ---
+m = leafmap.Map(center=[0, 0], zoom=2)
+# --- 4. ò/W ---
+# ò/|W (COG)
+m.add_raster(
+cog_url,
+palette="terrain", # o "terrain" () 
+layer_name="Global DEM (Raster)"
+)
+# ò/UßW (GDF)
+m.add_gdf(
+gdf,
+layer_name="_} (Vector)",
+style={"fillOpacity": 0, "color": "black", "weight": 0.5} # ún,}
+)
+# --- 5. ×v ---
+m.add_layer_control()
+m.to_streamlit(height=700)
