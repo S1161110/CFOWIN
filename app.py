b5a1591d543c5a8872ba294d9cf214dc84479428
S1161110 <s1161110@gm.ncue.edu.tw>
2025-10-22 02:30:32 +0000

    try

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ with st.sidebar:
     st.header("這裡是側邊欄")
 
 # 選擇框 (Selectbox)
-option = st.selectbox("你最喜歡的 GIS 軟體?",("QGIS", "ArcGIS", "ENVI", "GRASS"))
+    option = st.selectbox("你最喜歡的 GIS 軟體?",("QGIS", "ArcGIS", "ENVI", "GRASS"))
 
 # 滑桿 (Slider)
-year = st.slider("選擇一個年份:", 1990, 2030, 2024)
+    year = st.slider("選擇一個年份:", 1990, 2030, 2024)
 
 # 2. 在主頁面顯示 Widgets 的結果
 st.write(f"你選的軟體是: {option}")
