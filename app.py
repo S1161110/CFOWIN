fe8997c0f5881e06ea12bbfa0cc8baad98a7eea5
S1161110 <s1161110@gm.ncue.edu.tw>
2025-10-22 02:47:27 +0000

    更新頁面

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
 import streamlit as st
-import pandas as pd
-
-st.title("Streamlit  Widgets")
-
-# 1. 把 Widgets 放到側邊欄 (sidebar)
+# 1. o st.Page() ÿ¿¯
+# ÿst.Page() ¯×~ .py þ
+# Emoji Wÿhttps://tw.piliapp.com/emoji/list/
+pages = [
+st.Page("page_home.py", title="專案首頁", icon="👻"),
+st.Page("page_map.py", title="互動地圖瀏覽", icon="🫤"),
+st.Page("page_about.py", title="關於我們", icon="🫵")
+]
+# 2. o st.navigation() ½ (Ït)
 with st.sidebar:
-    st.header("這裡是側邊欄")
-    # 選擇框 (Selectbox)
-    option = st.selectbox("你最喜歡的 GIS 軟體?",("QGIS", "ArcGIS", "ENVI", "GRASS"))
-    # 滑桿 (Slider)
-    year = st.slider("選擇一個年份:", 1990, 2030, 2024)
-
-# 2. 在主頁面顯示 Widgets 的結果
-st.write(f"你選的軟體是: {option}")
-st.write(f"你選的年份是: {year}")
-
-# 按鈕 (Button)
-if st.button("點我顯示氣球!"):
-    st.balloons()
-
-# 檔案上傳 (File Uploader) - 地理系必備!
-uploaded_file = st.file_uploader("上傳你的 Shapefile (.zip) 或 GeoTIFF (.tif) 或 GeoJSON (.json)",
-type=["zip", "tif", "json"])
-
-if uploaded_file is not None:
-    st.success(f"你上傳了: {uploaded_file.name} (//: {uploaded_file.size} bytes)")
+    st.title("App導覽")
+    # st.navigation() Þóö¿¯
+    selected_page = st.navigation(pages)
+# 3. ¯ö¿¯
+selected_page.run()
